3fc36ebeeeb65326b2dee86982ac56ed902fe6c7
Milind Dumbare <milind.dumbare@gmail.com>
2013-02-13 11:40:10 +0100

    Cleaning build files

--- kernel/.dnbd.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mnt/store/dnbd_optimize/dnbd/kernel/dnbd.ko := ld -m elf_x86_64 -r -o /mnt/store/dnbd_optimize/dnbd/kernel/dnbd.ko /mnt/store/dnbd_optimize/dnbd/kernel/dnbd.o /mnt/store/dnbd_optimize/dnbd/kernel/dnbd.mod.o
